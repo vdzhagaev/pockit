626e00201eabcddd583e7867d3cfbb963db8248b
vyacheslav <vyacheslav@comradeweb.com>
2026-01-13 03:16:44 +0300

    feat(lib): add API response helpers and random alias generator

---
+++ internal/lib/random/random.go
@@ -0,0 +1,49 @@
+package random
+
+import (
+	"crypto/rand"
+)
+
+type Generator struct {
+	alphabet string
+	length   int
+}
+
+func (g *Generator) Generate() (string, error) {
+	bytes := make([]byte, g.length)
+	alphabetBytes := []byte(g.alphabet)
+	alphabetLen := byte(len(alphabetBytes))
+
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	for i := range bytes {
+		bytes[i] = alphabetBytes[bytes[i]%alphabetLen]
+	}
+
+	return string(bytes), nil
+}
+
+func NewGenerator(alphabet string, length int) *Generator {
+	if alphabet == "" {
+		alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	}
+	return &Generator{
+		alphabet: alphabet,
+		length:   length,
+	}
+}
+
+func NewRandomString(size int) (string, error) {
+	const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
+	generator := NewGenerator(alphabet, size)
+
+	str, err := generator.Generate()
+
+	if err != nil {
+		return "", err
+	}
+
+	return str, nil
+}
